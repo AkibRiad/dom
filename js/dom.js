5f1c29d3c0955b1a4e1fb1f6bac890759981af63
AkibRiad <akibriad652@gmail.com>
2024-10-11 16:09:35 +0600

    added something

--- js/dom.js
+++ js/dom.js
@@ -4,5 +4,11 @@ console.log("Start DOM");
 
  let li =document.getElementsByTagName("li");
  for(l of li){
-    console.log(l)
+   //  console.log(l)
  }
+
+ const change = document.getElementById("ab");
+
+ change.innerText = 'My Favourite Fruits'
+
+ change.setAttribute = ('title','It is JS')
